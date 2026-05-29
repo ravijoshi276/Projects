8249cf9cea7c89a9e623ccae02e72ea1b30134b3
Ravi Joshi <joshiravi2706@gmail.com>
2026-05-29 07:21:45 +0000

    Adeede django api project BookList

---
+++ Web Development/djangoprojects/BookList/BookListAPI/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books',views.books),
+    # path('books/<int:pk>',views.book),
+]
+
+# Solution code for urls.py (app-level):
