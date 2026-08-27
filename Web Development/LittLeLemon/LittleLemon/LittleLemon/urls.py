fca5cfb62017bd9aff592c77c8faa8348f53d5b5
Ravi Joshi <joshiravi2706@gmail.com>
2026-08-27 18:31:06 +0530

    Added some minor changes

--- Web Development/LittLeLemon/LittleLemon/LittleLemon/urls.py
+++ Web Development/LittLeLemon/LittleLemon/LittleLemon/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf import settings
+from django.conf import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
