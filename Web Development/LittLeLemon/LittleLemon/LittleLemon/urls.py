9f3a1d019ca5a4b2dd3076794b8905c58249eb36
Ravi Joshi <joshiravi2706@gmail.com>
2026-08-27 18:23:33 +0530

    Modified urls.py

--- Web Development/LittLeLemon/LittleLemon/LittleLemon/urls.py
+++ Web Development/LittLeLemon/LittleLemon/LittleLemon/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +25,12 @@ urlpatterns = [
    
 
     path('api/',include('LittleLemonDRF.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
+
 ]
+
+if settings.DEBUG:
+    # This prevents Render from evaluating debug modules when running in production
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
 
