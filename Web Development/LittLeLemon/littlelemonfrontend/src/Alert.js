661a60df567ba355fdc0888b1f463e7496246a90
Ravi Joshi <joshiravi2706@gmail.com>
2026-08-27 23:04:11 +0530

    Added minor changes on previous commit

--- Web Development/LittLeLemon/littlelemonfrontend/src/Alert.js
+++ Web Development/LittLeLemon/littlelemonfrontend/src/Alert.js
@@ -1,12 +1,16 @@
 
-export default function Alert ({type,heading,message}){
-    const Card = type==='success'?(<div class="fixed w-full bg-green-100 border-t border-b border-green-500 text-green-700 px-4 py-3" role="alert">
-  <p class="font-bold">{heading}</p>
-  <p class="text-sm">{message}</p>
-</div>):(<div class="fixed w-full bg-red-100 border-t border-b border-red-500 text-red-700 px-4 py-3" role="alert">
-  <p class="font-bold">{heading}</p>
-  <p class="text-sm">{message}</p>
-</div>) 
-    return Card
-
-};
+export default function Alert({ type, heading, message }) {
+    const Card = type === 'success' ? (
+        <div className="fixed w-full bg-green-100 border-t border-b border-green-500 text-green-700 px-4 py-3 z-50" role="alert">
+            <p className="font-bold">{heading}</p>
+            <p className="text-sm">{message}</p>
+        </div>
+    ) : (
+        <div className="fixed w-full bg-red-100 border-t border-b border-red-500 text-red-700 px-4 py-3 z-50" role="alert">
+            <p className="font-bold">{heading}</p>
+            <p className="text-sm">{message}</p>
+        </div>
+    );
+    
+    return Card;
+}
