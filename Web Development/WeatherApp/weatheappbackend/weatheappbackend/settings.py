78c01db9a87e02b114dd4c49b8162b39eb2c6bf8
Ravi Joshi <joshiravi2706@gmail.com>
2026-09-24 16:36:52 +0530

    Added static root path

--- Web Development/WeatherApp/weatheappbackend/weatheappbackend/settings.py
+++ Web Development/WeatherApp/weatheappbackend/weatheappbackend/settings.py
@@ -17,6 +17,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
